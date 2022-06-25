329b7dab7b8c9e85cdd5ea3ce1b8fa0b2f14e087
邱洋智超 <80151069+qyz1314404@users.noreply.github.com>
2022-06-25 20:11:05 +0800

    Update GitTest.java

--- src/main/java/cn/qy/git/GitTest.java
+++ src/main/java/cn/qy/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix commit2!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
