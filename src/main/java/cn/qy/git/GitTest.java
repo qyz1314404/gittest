d9c624b358780a4d1ddcd29b5b167c11e1341eeb
Qiuyang <1206857991@qq.com>
2022-06-25 16:53:36 +0800

    first commit

---
+++ src/main/java/cn/qy/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.qy.git;
+
+/**
+ * @description:
+ * @author: QiuYang
+ * @time: 2022/6/25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
