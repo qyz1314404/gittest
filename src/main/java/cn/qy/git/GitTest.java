c933476b07201dc0a10223ce1b323ab8459d6e1f
Qiuyang <1206857991@qq.com>
2022-06-25 16:56:59 +0800

    second commit

--- src/main/java/cn/qy/git/GitTest.java
+++ src/main/java/cn/qy/git/GitTest.java
@@ -8,5 +8,6 @@ package cn.qy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
